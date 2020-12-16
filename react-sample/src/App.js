fdf6c27feeb6b867ef66ec6e4abd7debcea7a039
ri003074 <ri003074@gmail.com>
2020-12-16 18:28:42 +0900

    create function component

--- react-sample/src/App.js
+++ react-sample/src/App.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
+import React, { } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-          <div>Hello World</div>
-      </div>
-    );
-  }
+//class App extends Component {
+//  render() {
+//    return (
+//      <div>
+//          <div>Hello World</div>
+//      </div>
+//    );
+//  }
+//}
+const App = () => {
+  return <div>
+            <Cat/>
+            <Cat/>
+            <Cat/>
+          </div>
+}
+
+const Cat = () => {
+  return <div>nya!</div>
 }
 
 export default App;
